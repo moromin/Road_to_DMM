53b6b882c070d9e90f7f3cf0ff3c765aa7b10c61
moromin <34294957+moromin@users.noreply.github.com>
2021-12-12 23:15:28 +0900

    Add: comment to createAccout()

--- app/domain/repository/account.go
+++ app/domain/repository/account.go
@@ -9,5 +9,7 @@ import (
 type Account interface {
 	// Fetch account which has specified username
 	FindByUsername(ctx context.Context, username string) (*object.Account, error)
+
+	// Create an account which has specified username and password
 	CreateAccount(ctx context.Context, username, password string) error
 }
