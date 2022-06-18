12d574d49e9a0e1f0a95585e99492ea63d2e0456
moromin <moromin933@gmai.com>
2022-06-18 21:49:14 +0900

    feat: add follow table to ddl

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -19,3 +19,15 @@ CREATE TABLE `status` (
   INDEX `idx_account_id` (`account_id`),
   CONSTRAINT `fk_status_account_id` FOREIGN KEY (`account_id`) REFERENCES  `account` (`id`)
 );
+
+-- TODO: automate migration
+CREATE TABLE `follows` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `follower_id` bigint(20) NOT NULL,
+  `followee_id` bigint(20) NOT NULL,
+  INDEX `idx_follower_id` (`follower_id`),
+  INDEX `idx_followee_id` (`followee_id`),
+  CONSTRAINT `fk_follower_account_id` FOREIGN KEY (`follower_id`) REFERENCES  `account` (`id`),
+  CONSTRAINT `fk_followee_account_id` FOREIGN KEY (`followee_id`) REFERENCES  `account` (`id`),
+  PRIMARY KEY (`id`)
+);
