a50eda60ff7e3fb3cac9956f9e46250de7fb6cda
moromin <moromin933@gmai.com>
2022-06-19 18:44:07 +0900

    feat: add follow endpoint

--- app/dao/account.go
+++ app/dao/account.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"log"
 	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/domain/repository"
 
@@ -67,4 +69,86 @@ func (r *account) CreateAccount(ctx context.Context, username, password string) 
 	}
 
 	return nil
+}
+
+// Follow : アカウントをフォロー
+func (r *account) Follow(ctx context.Context, follower_id, followee_id int64) (int64, bool, error) {
+	var id int64
+	var followedBy bool
+
+	err := r.Transaction(func(tx *sqlx.Tx) error {
+		const follow = `INSERT INTO follow (follower_id, followee_id) VALUES (?, ?)`
+		res, err := tx.ExecContext(ctx, follow, follower_id, followee_id)
+		if err != nil {
+			return err
+		}
+		id, err = res.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		if err := r.manageNumberOfFollows(ctx, tx, follower_id, "following_count", 1); err != nil {
+			return err
+		}
+		if err := r.manageNumberOfFollows(ctx, tx, followee_id, "followers_count", 1); err != nil {
+			return err
+		}
+
+		const followed = `SELECT * FROM follow WHERE follower_id = ? AND followee_id = ?`
+		empty := struct{ I, J, K int64 }{}
+		if err := r.db.QueryRowxContext(ctx, followed, followee_id, follower_id).Scan(&empty.I, &empty.J, &empty.K); err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				followedBy = false
+			} else {
+				return err
+			}
+		} else {
+			followedBy = true
+		}
+
+		return nil
+	})
+	if err != nil {
+		return 0, false, err
+	}
+
+	return id, followedBy, nil
+}
+
+func (r *account) manageNumberOfFollows(ctx context.Context, tx *sqlx.Tx, id int64, column string, number int64) error {
+	var operator string
+	if number == 0 {
+		return nil
+	} else if number > 0 {
+		operator = "+"
+	} else {
+		operator = "-"
+		number *= -1
+	}
+
+	query := fmt.Sprintf("UPDATE account SET %s = %s %s %d WHERE id = %d", column, column, operator, number, id)
+	_, err := tx.ExecContext(ctx, query)
+
+	return err
+}
+
+// Transaction handle specific process
+// Essentially, it should be abstracted by DB interface
+func (r *account) Transaction(txFunc func(*sqlx.Tx) error) error {
+	tx, err := r.db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err != nil {
+			log.Println("rollback")
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	err = txFunc(tx)
+	return err
 }
